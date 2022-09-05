c928520c9a2e9a3bccaf975aa243b1e77074dc3e
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2022-09-05 17:15:48 -0500

    se añade icono de clima dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  
+  const [weather,setWeather]= useState ({});
+
+
   useEffect(()=>{
   navigator.geolocation.getCurrentPosition(success);
-
+  
   function success(pos) {
+    
     const crd = pos.coords;
     console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
     console.log(`More or less ${crd.accuracy} meters.`);
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=1be5a17c97513a6ea873cbddde7fd6c7`).then(res=> console.log(res.data));
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=1be5a17c97513a6ea873cbddde7fd6c7`)
+    .then(res =>setWeather(res.data));
+   
+    
   }
 
 
@@ -22,6 +30,9 @@ function App() {
   return (
     <div className="App">
       <h1>hola mundo</h1>
+
+      {/* añadir imagen en weather */}
+        <img src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}@4x.png`} alt="" />
     </div>
   )
 }
