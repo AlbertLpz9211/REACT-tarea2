6b94246f28cb9322c39fb439019122558f854018
AlbertLpz9211 <alberto.lopez_g@outlook.com>
2022-09-06 03:05:45 -0500

    se anexa descripcion de climas

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ function App() {
       <div className="card">
       <h1>Weather Master App</h1>
       <h2>{weather?.name}, {weather.sys?.country}</h2>
+      <h2>{weather.weather[0]?.description}</h2>
       <IconWeather weather={weather}/>
       {console.log(weather)}
       </div>
